123b56aae41c509b13179899e3ee98270bfab0d4
Dotuan17012001 <dovantuan17012001@gmail.com>
2023-04-10 02:17:25 +0700

    video 52

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -3,7 +3,7 @@ import videoHomepage from "../../assets/video-homepage.mp4";
 const HomePage = (props) => {
   return (
     <div className="homepage-container">
-      <video>
+      <video autoPlay muted loop>
         <source src={videoHomepage} 
         type="video/mp4" />
       </video>
